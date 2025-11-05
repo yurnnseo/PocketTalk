6760f60805712fdc17a38ece391d5e2189b4e5f1
jei kwon <topjay52@gmail.com>
2025-11-05 15:29:46 +0900

    로그인 화면 변경

--- src/JavaChatClientMain.java
+++ src/JavaChatClientMain.java
@@ -2,9 +2,11 @@
 //Java Client 시작
 
 import java.awt.EventQueue;
+import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -41,47 +43,43 @@ public class JavaChatClientMain extends JFrame {
 	 */
 	public JavaChatClientMain() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 254, 321);
+		setBounds(100, 100, 420, 570);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JLabel lblNewLabel = new JLabel("User Name");
-		lblNewLabel.setBounds(12, 39, 82, 33);
-		contentPane.add(lblNewLabel);
-		
+		ImageIcon icon = new ImageIcon(getClass().getResource("login.png"));
+        Image scaledImage = icon.getImage().getScaledInstance(420, 570, Image.SCALE_SMOOTH);
+        ImageIcon scaledIcon = new ImageIcon(scaledImage);
+
+        JLabel imgLabel = new JLabel(scaledIcon);
+        imgLabel.setBounds(0, 0, 420, 570);
+        contentPane.add(imgLabel);
+        
 		txtUserName = new JTextField();
 		txtUserName.setHorizontalAlignment(SwingConstants.CENTER);
-		txtUserName.setBounds(101, 39, 116, 33);
-		contentPane.add(txtUserName);
+		txtUserName.setBounds(233, 180, 116, 33);
+		imgLabel.add(txtUserName);
 		txtUserName.setColumns(10);
-		
-		JLabel lblIpAddress = new JLabel("IP Address");
-		lblIpAddress.setBounds(12, 100, 82, 33);
-		contentPane.add(lblIpAddress);
 		
 		txtIpAddress = new JTextField();
 		txtIpAddress.setHorizontalAlignment(SwingConstants.CENTER);
 		txtIpAddress.setText("127.0.0.1");
 		txtIpAddress.setColumns(10);
-		txtIpAddress.setBounds(101, 100, 116, 33);
-		contentPane.add(txtIpAddress);
-		
-		JLabel lblPortNumber = new JLabel("Port Number");
-		lblPortNumber.setBounds(12, 163, 82, 33);
-		contentPane.add(lblPortNumber);
+		txtIpAddress.setBounds(233, 250, 116, 33);
+		imgLabel.add(txtIpAddress);
 		
 		txtPortNumber = new JTextField();
 		txtPortNumber.setText("30000");
 		txtPortNumber.setHorizontalAlignment(SwingConstants.CENTER);
 		txtPortNumber.setColumns(10);
-		txtPortNumber.setBounds(101, 163, 116, 33);
-		contentPane.add(txtPortNumber);
+		txtPortNumber.setBounds(233, 320, 116, 33);
+		imgLabel.add(txtPortNumber);
 		
-		JButton btnConnect = new JButton("Connect");
-		btnConnect.setBounds(12, 223, 205, 38);
-		contentPane.add(btnConnect);
+		JButton btnConnect = new JButton("LOGIN");
+		btnConnect.setBounds(108, 394, 205, 38);
+		imgLabel.add(btnConnect);
 		Myaction action = new Myaction();
 		btnConnect.addActionListener(action);
 		txtUserName.addActionListener(action);

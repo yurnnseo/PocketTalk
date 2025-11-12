1b9de9b3a98fd18a0041e73edb22275e9f8059d8
ShinYoungSeo <ShinYoungSeo@DESKTOP-NUUH4FQ>
2025-11-12 15:19:03 +0900

    o

--- src/ProfileEditPanel.java
+++ src/ProfileEditPanel.java
@@ -168,6 +168,7 @@ public class ProfileEditPanel extends JPanel {
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
         g.setColor(Color.BLACK); // 선 색상
-        g.fillRect(50, 200, getWidth() - 100, 2);
+        g.fillRect(29, 206, getWidth() - 65, 1);
+        g.fillRect(29, 262, getWidth() - 65, 1);
     }
 }
